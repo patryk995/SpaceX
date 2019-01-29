db8dd9b5e8bfbd2772faaa1a45daf9157b94b7c6
Patryk <patryk.bartosevic@gmail.com>
2019-01-29 22:37:19 +0200

    path typo correct

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
 const cors = require('cors');
-const path = require('./path');
+const path = require('path');
 
 const app = express();
 
